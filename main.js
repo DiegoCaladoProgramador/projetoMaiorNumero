4331899b602de03b94720a222defe753abd10880
DiegoCaladoProgramador <diegocaladoprogramador@gmail.com>
2023-03-27 18:21:18 -0300

    criação do index, css e js, fala corrigir algum erro

---
+++ main.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('numForms');
+const numUm = document.getElementById('numUm');
+const numdois = document.getElementById('numDois');
+let formEValido = false;
+
+function validaNumeros(nA, nB){
+    let numeroValido = false;
+    if(nB > nA){
+        numeroValido = true;
+    }
+    return numeroValido;
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    formEValido = validaNumeros(numUm.value, numdois.value)
+    const mensagemSucesso = 'O número dois é maior que o número um'
+    if(formEValido){
+        const containerMensagemSucesso = document.querySelector('.mensagemSucesso');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block'
+
+        numUm.value = ''
+        numdois.value = ''
+    } else {
+        numdois.style.border = '1px solid red'
+        document.querySelector('.mensagemErro').style.display = 'block';
+    }
+})
+
+numdois.addEventListener('keyup', function(e){
+    formEValido = validaNumeros(numUm.value, e.target.value);
+    if (!formEValido){
+        numdois.classList.add('error');
+        document.querySelector('.mensagemErro').style.display = 'block';
+    }else{
+        numdois.classList.remove('error');
+        document.querySelector('.mensagemErro').style.display = 'none';
+    }
+})
