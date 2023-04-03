196bfa20b7802c8e18842d296aa4a8fde4573801
DiegoCaladoProgramador <diegocaladoprogramador@gmail.com>
2023-04-03 17:03:28 -0300

    corrigido bug que sempre aparece mensagem de erro.

--- main.js
+++ main.js
@@ -17,14 +17,15 @@ form.addEventListener('submit', function(e){
     const mensagemSucesso = 'O número dois é maior que o número um'
     if(formEValido){
         const containerMensagemSucesso = document.querySelector('.mensagemSucesso');
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
-        containerMensagemSucesso.style.display = 'block'
-
+        if(containerMensagemSucesso){
+            containerMensagemSucesso.innerHTML = mensagemSucesso;
+            containerMensagemSucesso.style.display = 'block'
+        }
         numUm.value = ''
         numdois.value = ''
     } else {
         numdois.style.border = '1px solid red'
-        document.querySelector('.mensagemErro').style.display = 'block';
+        document.querySelector('#mensagemErro').style.display = 'block';
     }
 })
 
@@ -32,9 +33,9 @@ numdois.addEventListener('keyup', function(e){
     formEValido = validaNumeros(numUm.value, e.target.value);
     if (!formEValido){
         numdois.classList.add('error');
-        document.querySelector('.mensagemErro').style.display = 'block';
+        document.querySelector('#mensagemErro').style.display = 'block';
     }else{
         numdois.classList.remove('error');
-        document.querySelector('.mensagemErro').style.display = 'none';
+        document.querySelector('#mensagemErro').style.display = 'none';
     }
 })
